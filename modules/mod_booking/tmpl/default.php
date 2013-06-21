d6cf77c5f12caac20ce851abbd44ee59920d3e84
Konstantin Kudinov <kudinov.k@gmail.com>
2013-06-21 19:42:37 +0600

    booking form dialog

--- modules/mod_booking/tmpl/default.php
+++ modules/mod_booking/tmpl/default.php
@@ -26,6 +26,9 @@ $doc->addScriptDeclaration("
 				}
 			}).done(function(json) {
 				$('#booking_cart').html(json.result);
+				$( '#order_cart' ).button().click(function() {
+					$( '#dialog-form' ).dialog( 'open' );
+				});
 			});
 		}
 	};
@@ -71,6 +74,20 @@ $doc->addScriptDeclaration("
 ?>
 <div id="booking_cart" class="stats-module<?php echo $moduleclass_sfx; ?>">
 
+</div>
+
+<div id="dialog-form" title="Create new user">
+	 <p class="validateTips">All form fields are required.</p>
+	<form>
+		<fieldset>
+			<label for="name">Name</label>
+			<input type="text" name="name" id="name" class="text ui-widget-content ui-corner-all" />
+			<label for="email">Email</label>
+			<input type="text" name="email" id="email" value="" class="text ui-widget-content ui-corner-all" />
+			<label for="password">Password</label>
+			<input type="password" name="password" id="password" value="" class="text ui-widget-content ui-corner-all" />
+		</fieldset>
+	</form>
 </div>
 
  <script>
@@ -124,7 +141,6 @@ $doc->addScriptDeclaration("
 				bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
 				bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
 				if ( bValid ) {
-					alert('OK');
 					$( this ).dialog( "close" );
 				}
 			},
@@ -137,25 +153,11 @@ $doc->addScriptDeclaration("
 		}
 	});
 
-	$( "#order_cart" ).button().click(function() {
-		$( "#dialog-form" ).dialog( "open" );
-	});
 })(jQuery);
 </script>
 
 
-<div id="dialog-form" title="Create new user">
-	<form>
-		<fieldset>
-			<label for="name">Name</label>
-			<input type="text" name="name" id="name" class="text ui-widget-content ui-corner-all" />
-			<label for="email">Email</label>
-			<input type="text" name="email" id="email" value="" class="text ui-widget-content ui-corner-all" />
-			<label for="password">Password</label>
-			<input type="password" name="password" id="password" value="" class="text ui-widget-content ui-corner-all" />
-		</fieldset>
-	</form>
-</div>
+
 
 <script type="text/javascript">
 jQuery(document).ready(function() {
