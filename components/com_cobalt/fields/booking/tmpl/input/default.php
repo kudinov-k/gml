5944a8c7d98cc24f2c9b607cee0bfd3ba0a9d67a
Konstantin Kudinov <kudinov.k@gmail.com>
2013-08-27 18:42:20 +0600

    booking

--- components/com_cobalt/fields/booking/tmpl/input/default.php
+++ components/com_cobalt/fields/booking/tmpl/input/default.php
@@ -29,7 +29,63 @@ if (!isset($this->value['sale']))
 {
 	$this->value['sale'] = 0;
 }
+if (!isset($this->value['order']))
+{
+	$this->value['order'] = 0;
+}
 ?>
+
+<table>
+	<thead>
+		<tr>
+			<th>Операция</th>
+			<th>Баз.ед.изм</th>
+			<th>Время(ч)</th>
+			<th>Цена</th>
+			<th>Ед.время</th>
+			<th>Скидка</th>
+			<th>Налог</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>Аренда</td>
+			<td>
+				<select name="jform[fields][<?php echo $this->id;?>][rent][unit]" style="width:70px;">
+					<?php
+					$units = explode("\n", $this->params->get('params.unit', ''));
+					foreach ($units as $key => $unit):?>
+						<option
+							<?php  if(isset($this->value['rent']['unit']) && $this->value['rent']['unit'] == $key):?>
+								selected="selected"
+							<?php endif;?>
+
+							value="<?php echo $key;?>"><?php echo $unit;?>
+						</option>
+					<?php endforeach;?>
+				</select>
+			</td>
+			<td>
+				<input type="checkbox" />
+			</td>
+			<td>
+				<select name="jform[fields][<?php echo $this->id;?>][rent][time]" style="width:70px;">
+					<?php
+					$times = explode("\n", $this->params->get('params.times', ''));
+					foreach ($times as $key => $time):?>
+						<option
+							<?php  if(isset($this->value['rent']['unit']) && $this->value['rent']['time'] == $key):?>
+								selected="selected"
+							<?php endif;?>
+
+							value="<?php echo $key;?>"><?php echo $time;?>
+						</option>
+					<?php endforeach;?>
+				</select>
+			</td>
+		</tr>
+	</tbody>
+</table>
 
 <?php echo JText::_('CRENT');?>
 <input type="text" name="jform[fields][<?php echo $this->id;?>][rent]" id="field_<?php echo $this->id;?>" value="<?php echo $this->value['rent'];?>"
@@ -37,5 +93,9 @@ if (!isset($this->value['sale']))
 	<br /><br />
 <?php echo JText::_('CSALE');?>
 <input type="text" name="jform[fields][<?php echo $this->id;?>][sale]" id="field_<?php echo $this->id;?>" value="<?php echo $this->value['sale'];?>"
+	<?php echo $class .$required;?>>
+	<br /><br />
+<?php echo JText::_('CORDER');?>
+<input type="text" name="jform[fields][<?php echo $this->id;?>][order]" id="field_<?php echo $this->id;?>" value="<?php echo $this->value['order'];?>"
 	<?php echo $class .$required;?>>
 
