c1891f45bf34f190771b446a24017831bc5bb128
Konstantin Kudinov <kudinov.k@gmail.com>
2013-09-02 08:49:28 +0600

    booking

--- components/com_cobalt/fields/booking/tmpl/input/default.php
+++ components/com_cobalt/fields/booking/tmpl/input/default.php
@@ -19,31 +19,30 @@ if ($this->required)
 
 $class = ' class="' . implode(' ', $class) . '"';
 
-settype($this->value, 'array');
+//var_dump($this->value);
+// settype($this->value, 'array');
 
-if (!isset($this->value['rent']))
-{
-	$this->value['rent'] = isset($this->value[0]) ? $this->value[0] : 1;
-}
-if (!isset($this->value['sale']))
-{
-	$this->value['sale'] = 0;
-}
-if (!isset($this->value['order']))
-{
-	$this->value['order'] = 0;
-}
+// if (!isset($this->value['rent']))
+// {
+// 	$this->value['rent'] = isset($this->value[0]) ? $this->value[0] : 1;
+// }
+// if (!isset($this->value['sale']))
+// {
+// 	$this->value['sale'] = 0;
+// }
+// if (!isset($this->value['order']))
+// {
+// 	$this->value['order'] = 0;
+// }
 ?>
 
-<table>
+<table class="table">
 	<thead>
 		<tr>
 			<th>Операция</th>
 			<th>Баз.ед.изм</th>
 			<th>Время(ч)</th>
 			<th>Цена</th>
-			<th>Ед.время</th>
-			<th>Скидка</th>
 			<th>Налог</th>
 		</tr>
 	</thead>
@@ -69,33 +68,112 @@ if (!isset($this->value['order']))
 				<input type="checkbox" />
 			</td>
 			<td>
-				<select name="jform[fields][<?php echo $this->id;?>][rent][time]" style="width:70px;">
+				<table class="table">
+				<?php
+				$times = explode("\n", $this->params->get('params.times', ''));
+				foreach ($times as $key => $time):
+					$v = null;
+					if(isset($this->value['rent']['price'][$key]))
+					{
+						$v = $this->value['rent']['price'][$key];
+					}
+				?>
+					<tr>
+					<td><?php echo $time;?></td>
+					<td><input type="text" value="<?php echo $v;?>" class="input-mini" name="jform[fields][<?php echo $this->id;?>][rent][price][<?php echo $key;?>]" /></td>
+					</tr>
+				<?php endforeach;?>
+				</table>
+			</td>
+			<td>
+				<select name="jform[fields][<?php echo $this->id;?>][tax][]" multiple="multiplr" style="width:150px;">
 					<?php
-					$times = explode("\n", $this->params->get('params.times', ''));
-					foreach ($times as $key => $time):?>
+					$units = explode("\n", $this->params->get('params.tax', ''));
+					foreach ($units as $key => $unit):?>
 						<option
-							<?php  if(isset($this->value['rent']['unit']) && $this->value['rent']['time'] == $key):?>
+							<?php  if(isset($this->value['tax'])/* && $this->value == $key*/):?>
 								selected="selected"
 							<?php endif;?>
 
-							value="<?php echo $key;?>"><?php echo $time;?>
+							value="<?php echo $key;?>"><?php echo $unit;?>
 						</option>
 					<?php endforeach;?>
 				</select>
 			</td>
 		</tr>
+
+		<tr>
+			<td>Продажа</td>
+			<td>
+				<select name="jform[fields][<?php echo $this->id;?>][sale][unit]" style="width:70px;">
+					<?php
+					$units = explode("\n", $this->params->get('params.unit', ''));
+					foreach ($units as $key => $unit):?>
+						<option
+							<?php  if(isset($this->value['sale']['unit']) && $this->value['sale']['unit'] == $key):?>
+								selected="selected"
+							<?php endif;?>
+
+							value="<?php echo $key;?>"><?php echo $unit;?>
+						</option>
+					<?php endforeach;?>
+				</select>
+			</td>
+			<td>
+
+			</td>
+			<td>
+				<?php
+					$v = null;
+					if(isset($this->value['sale']['price']))
+					{
+						$v = $this->value['sale']['price'];
+					}
+				?>
+				<input type="text" value="<?php echo $v;?>" class="input-mini" name="jform[fields][<?php echo $this->id;?>][sale][price]" />
+			</td>
+		</tr>
+
+		<tr>
+			<td>Заказ</td>
+			<td>
+				<select name="jform[fields][<?php echo $this->id;?>][order][unit]" style="width:70px;">
+					<?php
+					$units = explode("\n", $this->params->get('params.unit', ''));
+					foreach ($units as $key => $unit):?>
+						<option
+							<?php  if(isset($this->value['order']['unit']) && $this->value['order']['unit'] == $key):?>
+								selected="selected"
+							<?php endif;?>
+
+							value="<?php echo $key;?>"><?php echo $unit;?>
+						</option>
+					<?php endforeach;?>
+				</select>
+			</td>
+			<td>
+				<input type="checkbox" />
+			</td>
+			<td>
+				<table>
+				<?php
+				$times = explode("\n", $this->params->get('params.times', ''));
+				foreach ($times as $key => $time):
+					$v = null;
+					if(isset($this->value['order']['price'][$key]))
+					{
+						$v = $this->value['order']['price'][$key];
+					}
+				?>
+					<tr>
+					<td><?php echo $time;?></td>
+					<td><input type="text" value="<?php echo $v;?>" class="input-mini" name="jform[fields][<?php echo $this->id;?>][order][price][<?php echo $key;?>]" /></td>
+					</tr>
+				<?php endforeach;?>
+				</table>
+			</td>
+
+		</tr>
 	</tbody>
 </table>
-
-<?php echo JText::_('CRENT');?>
-<input type="text" name="jform[fields][<?php echo $this->id;?>][rent]" id="field_<?php echo $this->id;?>" value="<?php echo $this->value['rent'];?>"
-	<?php echo $class .$required;?>>
-	<br /><br />
-<?php echo JText::_('CSALE');?>
-<input type="text" name="jform[fields][<?php echo $this->id;?>][sale]" id="field_<?php echo $this->id;?>" value="<?php echo $this->value['sale'];?>"
-	<?php echo $class .$required;?>>
-	<br /><br />
-<?php echo JText::_('CORDER');?>
-<input type="text" name="jform[fields][<?php echo $this->id;?>][order]" id="field_<?php echo $this->id;?>" value="<?php echo $this->value['order'];?>"
-	<?php echo $class .$required;?>>
 
